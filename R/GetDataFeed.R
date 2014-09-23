77b9d386241b4c7992229bc15b4dd89a443a244a
Per Kaer <evilper@gmail.com>
2014-09-23 12:43:16 +0200

    fixed error msg bug and inserted a small time delay to avoid google limit

--- R/GetDataFeed.R
+++ R/GetDataFeed.R
@@ -1,21 +1,21 @@
-#' This will request with the prepared Query to the Google Analytics 
+#' This will request with the prepared Query to the Google Analytics
 #' Data feed API and returns the data in dataframe R object.
-#' 
-#' @keywords internal 
-#' 
-#' @param query.uri The URI prepared by the QueryBuilder class.   
-#' 
-#' @return GA.list The Google Analytics API JSON response converted to a 
+#'
+#' @keywords internal
+#'
+#' @param query.uri The URI prepared by the QueryBuilder class.
+#'
+#' @return GA.list The Google Analytics API JSON response converted to a
 #' list object
-#' 
+#'
 #' @importFrom httr GET
 GetDataFeed <- function(query.uri) {
-  
-  GA.Data <- GET(query.uri)  
+
+  GA.Data <- GET(query.uri)
   GA.list <- ParseDataFeedJSON(GA.Data)
   if (is.null(GA.list$rows)) {
-    cat("Your query matched 0 results. Please verify your query using the Query Feed Explorer and re-run it.")
-    break
+    stop("Your query matched 0 results. Please verify your query using the Query Feed Explorer and re-run it.")
+#     GA.list$rows <- list(as.list(rep(NA, length(GA.list$columnHeaders))))
   } else {
     return (GA.list)
   }
--- R/SplitQueryDaywise.R
+++ R/SplitQueryDaywise.R
@@ -1,50 +1,52 @@
 #' This function breaks up the time range (as specified by Start Date and End Date) into single days
 #' and hits a query for each day. The responses for each queries are collated into a single dataframe and
 #' returned. This procedure helps decrease the effect of sampling.
-#' 
+#'
 #' @seealso https://developers.google.com/analytics/devguides/reporting/core/v2/gdataReferenceDataFeed#largeDataResults
-#' 
-#' @keywords internal 
-#' 
+#'
+#' @keywords internal
+#'
 #' @param query.builder Name of the object corresponding to the QueryBuilder class
-#' @param token Token Object created by the Auth() function   
+#' @param token Token Object created by the Auth() function
 #' @param delay Time delay in seconds between successive queries in order to avoid Rate Limit Error
-#' 
-#' @return list containing the Column Headers and the Collated dataframe that represents the query response 
-#' 
+#'
+#' @return list containing the Column Headers and the Collated dataframe that represents the query response
+#'
 #' @importFrom lubridate ymd
 #' @importFrom lubridate days
 
 SplitQueryDaywise <- function(query.builder, token, delay) {
-     
+
   kMaxDefaultRows <- get("kMaxDefaultRows",envir = rga.environment)
-  
+
   # Validate the token and regenerate it if expired
   ValidateToken(token)
-  
+
   # Updated to use Get Methods
   start.date <- ymd(query.builder$GetStartDate())
   end.date   <- ymd(query.builder$GetEndDate())
-  
+
   date.difference <- as.numeric(difftime(end.date, start.date, units = 'days'))
-  
+
   if (date.difference == 0) {
     stop("Please verify start date and end date. They cannot be the same")
   }
-  
+
   # Create an empty dataframe in order to store the results
   master.df <- data.frame()
-  
+
   for (i in (0:date.difference)) {
+    # to avoid hitting google limit on max 10 queries/second
+    Sys.sleep(0.11)
     # Update the start and end dates in the query
     date <- format(as.POSIXct(start.date) + days(i), '%Y-%m-%d')
     cat("[ Run", i, "of", date.difference, "] Getting data for", date, "\n")
     query.builder$SetStartDate(date)
     query.builder$SetEndDate(date)
-    
+
     # Reset the start index to 1 since a new query will be fired for a different date-range
     query.builder$SetStartIndex(as.numeric(1))
-    
+
     # Hit the first query corresponding the particular date
     first.query.df <- data.frame()
     inter.df <- data.frame()
@@ -52,9 +54,9 @@ SplitQueryDaywise <- function(query.builder, token, delay) {
     Sys.sleep(delay)
     first.query <- GetDataFeed(query.uri)
     first.query.df <- rbind(first.query.df, do.call(rbind, as.list(first.query$rows)))
-    
+
     # Check if pagination is required in the query
-    
+
     if (length(first.query$rows) < first.query$totalResults) {
       number.of.pages <- ceiling((first.query$totalResults) / length(first.query$rows))
         if ((number.of.pages > 100) & exists("kMaxPages", envir = rga.environment))  {
@@ -68,6 +70,6 @@ SplitQueryDaywise <- function(query.builder, token, delay) {
       master.df <- rbind(first.query.df, master.df)
     }
   }
-  
+
   return(list(header = first.query$columnHeaders, data=master.df))
 }
